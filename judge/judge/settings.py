85e0d6ac3cc646ad44e3043f879eb802ee2b0b3c
Ali Jardine <jardine64@gmail.com>
2024-07-20 21:31:52 +0100

    updating settings to serve static files

--- judge/judge/settings.py
+++ judge/judge/settings.py
@@ -118,6 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# For development, use this
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+# For production, use this
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
--- judge/judge/urls.py
+++ judge/judge/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("", include("judgement.urls")),
     path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
