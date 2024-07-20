b5b262e0c839c205731c9d26f3247e30f3b62a1b
Ali Jardine <jardine64@gmail.com>
2024-07-20 21:06:49 +0100

    adding fully qualified domain

--- judge/judge/settings.py
+++ judge/judge/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '8)810zj@#^2xp=1=2rkozbv8#)gub6m1a^9qf&)d-9&x9*c2a_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codestuff.online']
+ALLOWED_HOSTS = ['codestuff.online','www.codestuff.online']
 
 
 # Application definition
