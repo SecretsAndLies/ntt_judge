d95eb04524f5bd073e7bfdaece44ef9926cc4324
Ali Jardine <jardine64@gmail.com>
2024-07-24 18:19:16 +0100

    removing some security stuff

--- judge/judge/settings.py
+++ judge/judge/settings.py
@@ -134,10 +134,3 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CSRF_TRUSTED_ORIGINS = ["https://codestuff.online", "https://www.codestuff.online"]
-
-SECURE_BROWSER_XSS_FILTER = True
-SECURE_CONTENT_TYPE_NOSNIFF = True
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-X_FRAME_OPTIONS = "DENY"
